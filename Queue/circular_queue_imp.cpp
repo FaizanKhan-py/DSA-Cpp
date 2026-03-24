8a80bf5d3c66e16c747a306451089edb08ab8e69
FaizanKhan-py <fk15233@gmail.com>
2026-03-24 16:13:40 +0500

    circular queue imp

---
+++ Queue/circular_queue_imp.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+class CircularQueue {
+private:
+    int* arr;
+    int front;
+    int rear;
+    int size;
+
+public:
+    CircularQueue(int s) {
+        size = s;
+        arr = new int[size];
+        front = -1;
+        rear = -1;
+    }
+
+    bool isFull() {
+        return (front == (rear + 1) % size);
+    }
+
+    bool isEmpty() {
+        return (front == -1);
+    }
+
+    void enqueue(int value) {
+        if (isFull()) {
+            cout << "Queue Overflow\n";
+            return;
+        }
+
+        if (front == -1) {
+            front = rear = 0;
+        }
+        else {
+            rear = (rear + 1) % size;
+        }
+
+        arr[rear] = value;
+    }
+
+    void dequeue() {
+        if (isEmpty()) {
+            cout << "Queue Underflow\n";
+            return;
+        }
+
+        if (front == rear) {
+            front = rear = -1;
+        }
+        else {
+            front = (front + 1) % size;
+        }
+    }
+
+    int getFront() {
+        if (isEmpty()) {
+            cout << "Queue is Empty\n";
+            return -1;
+        }
+        return arr[front];
+    }
+
+    void display() {
+        if (isEmpty()) {
+            cout << "Queue is Empty\n";
+            return;
+        }
+
+        int i = front;
+        while (true) {
+            cout << arr[i] << " ";
+            if (i == rear) break;
+            i = (i + 1) % size;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    CircularQueue q(5);
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+
+    q.display();
+
+    q.dequeue();
+    q.dequeue();
+
+    q.display();
+
+    q.enqueue(50);
+    q.enqueue(60);
+
+    q.display();
+
+    return 0;
+}
